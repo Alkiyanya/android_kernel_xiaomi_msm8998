3e0f729c064bdfe20ad52b9b3c0e07d2af357aa8
ShevT <shev_t@mail.ru>
2018-08-06 13:50:21 +0300

    sched: cputime: Fix compiling error if use SCHED_HMP

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -81,7 +81,9 @@ void irqtime_account_irq(struct task_struct *curr)
 	irq_time_write_end();
 
 	if (account) {
+#ifdef CONFIG_SCHED_WALT
 		walt_account_irqtime(cpu, curr, delta, wallclock);
+#endif
 		sched_account_irqtime(cpu, curr, delta, wallclock);
 	} else if (curr != this_cpu_ksoftirqd())
 		sched_account_irqstart(cpu, curr, wallclock);
